11713816ac797a962677ed6cc2579e39208b704a
Oleksandr Pyrohov <pilotrw@gmail.com>
2026-05-16 18:10:24 +0200

    add Alembic

--- app/db/database.py
+++ app/db/database.py
@@ -1,12 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import declarative_base
-
 from app.config.settings import settings
 
-
-engine = create_async_engine(
-    settings.DATABASE_URL,
-    echo=True,
-)
-
+engine = create_async_engine(settings.DATABASE_URL)
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
 
-from app.db.database import engine, Base
-from app.models.supplier_offer import SupplierOffer  # <- ВАЖЛИВО
-
 app = FastAPI()
-
-
-@app.on_event("startup")
-async def startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
 
 
 @app.get("/")
